f5f9a9978b883bab932dcb9063d2da4b5c3a889b
YASH VILAS KARANJULE <yashkaranjule230@gmail.com>
2024-10-24 11:28:52 +0530

    Title and remaning images added

--- project/project/urls.py
+++ project/project/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('index/',views.index),
     path('about/',views.about),
     path('contact/',views.contact),
-    path('features/',views.feature),
+    path('feature/',views.feature),
     path('project/',views.project),
     path('quote/',views.quote),
     path('service/',views.service),
