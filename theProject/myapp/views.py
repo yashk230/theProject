6c815f11354f45d481fd6335d43b7cb481f1e41a
YASH VILAS KARANJULE <yashkaranjule230@gmail.com>
2024-12-11 16:00:49 +0530

    new cahanges

---
+++ theProject/myapp/views.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.shortcuts import redirect, render
+
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def project(request):
+    return render(request,'project.html')
+
+def service(request):
+    return render(request,'service.html')
+
+def team(request):
+    return render(request,'team.html')
+
+def quote(request):
+    return render(request,'quote.html')
+
+def testimonial(request):
+    return render(request,'testimonial.html')
