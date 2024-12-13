fe84a51d8f86b3c6013de199e1c9066c42af301b
YASH VILAS KARANJULE <yashkaranjule230@gmail.com>
2024-12-13 22:34:07 +0530

    creating db tables

--- theProject/theProject/settings.py
+++ theProject/theProject/settings.py
@@ -79,13 +79,22 @@ WSGI_APPLICATION = 'theProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'theProject',
+        'USER':'root',
+        'HOST':'localhost',
+        'PASSWORD':'root123',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
