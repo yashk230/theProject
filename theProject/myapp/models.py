e1e2c854b98c2ac186ed043f931495718f16f185
YASH VILAS KARANJULE <yashkaranjule230@gmail.com>
2024-12-17 22:08:45 +0530

    minor fixing

--- theProject/myapp/models.py
+++ theProject/myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Projects(models.Model):
-    pname=models.CharField(max_length=20,verbose_name="Name of the Project")
+    pname=models.CharField(max_length=50,verbose_name="Name of the Project")
     pdesc=models.CharField(max_length=100,verbose_name="Description of the Project")
     pimage=models.ImageField(upload_to="project_images")
     price=models.IntegerField()
@@ -12,7 +12,7 @@ class Projects(models.Model):
         return self.pname
     
 class Services(models.Model):
-    sname=models.CharField(max_length=30,verbose_name="Name of the Service")
+    sname=models.CharField(max_length=50,verbose_name="Name of the Service")
     sdesc=models.CharField(max_length=150,verbose_name="Description of the Service")
     simage=models.ImageField(upload_to="service_image")
     sprice=models.IntegerField()
